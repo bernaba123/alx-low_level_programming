97aa736f51460150039608e5b3ebc93dd0f1366d
bernaba123 <70795558+bernaba123@users.noreply.github.com>
2021-02-04 05:19:45 -0800

    Create 10-print_comb2.c

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a = 0;
+int b = 0;
+while (a < 10)
+{
+while (b <= a)
+{
+putchar(b + '0');
+b++;
+putchar(',');
+}
+putchar(a + '0')
+a++
+}
+return (0);
+}
