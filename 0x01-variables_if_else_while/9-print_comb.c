f912e2344805ee7ad141765b2a5f111e888392ff
bernaba123 <70795558+bernaba123@users.noreply.github.com>
2021-02-04 06:47:54 -0800

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,15 +9,20 @@
 
 int main(void)
 {
-int a = 0;
-while (a < 10)
-{
-putchar(a + '0');
-a++;
-if (a < 10)
-putchar(',');
-putchar(' ');
-}
-putchar('\n');
-return (0);
+	int n = 48;
+
+	while (n < 58)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		n++;
+	}
+
+	putchar('\n');
+
+	return (0);
 }
