aa6d898d83459292e316eab22885c65057fd29f7
bernaba123 <70795558+bernaba123@users.noreply.github.com>
2021-02-08 00:57:38 -0800

    Create 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+ 
+void print_alphabet(void)
+{
+char c = 'a';
+while (c <= 'z')
+{
+_putchar(c);
+c++;
+}
+};
+
+int main(void)
+{
+print_alphabet();
+return (0);
+}
