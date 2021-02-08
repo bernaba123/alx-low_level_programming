a4f82812aff42b2c805aac233f2f94e762eb400a
bernaba123 <70795558+bernaba123@users.noreply.github.com>
2021-02-08 00:47:35 -0800

    Create 0-holberton.c

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,20 @@
+int _putchar(char c);
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+ 
+ int main(void)
+ {
+ int i;
+ char c[10] = "Holberton";
+ while (i < 9)
+ {
+ _putchar(c[i]);
+ i++;
+ }
+ _putchar('\n');
+ return (0);
+ }
