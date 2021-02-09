85277e1deccfbae4a2ada281f5c8c8b459c41f43
bernaba123 <70795558+bernaba123@users.noreply.github.com>
2021-02-08 23:50:07 -0800

    Create 11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,33 @@
+#include "holberton"
+#include <stdio.h>
+
+/**
+ * print_last_digit - Entry.
+ *
+ * @n: an integer argument
+ *
+ * Return: Always 0.
+ */
+ 
+void print_to_98(int n){
+	if (n>=98){
+		while(n>=98){
+			printf("%d", n);
+			n--;
+			if (n>=98)
+			printf(", ");
+		}
+	 printf("\n");
+	}
+	else if (n<=98)
+	{
+		while (n<=98){
+			printf("%d", n);
+			n++;
+			if (n<99)
+			printf(", ");
+			
+		}
+		printf("\n");
+	}
+}
