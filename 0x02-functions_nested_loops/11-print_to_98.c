d388a10cdb9a2478b686b359462a808447d06faf
bernaba123 <bernabasgetachew0946@gmail.com>
2021-02-09 08:19:21 +0000

    all

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,25 +8,26 @@
  */
 void print_to_98(int n)
 {
-if (n >= 98){
+if (n >= 98)
+{
 while (n >= 98)
 {
-printf ("%d", n);
+printf("%d", n);
 n--;
 if (n >= 98)
-printf (", ");
+printf(", ");
 }
-printf ("\n");
+printf("\n");
 }
 else if (n <= 98)
 {
 while (n <= 98)
 {
-printf ("%d", n);
+printf("%d", n);
 n++;
 if (n < 99)
-printf (", ");	
+printf(", ");
 }
-printf ("\n");
+printf("\n");
 }
 }
