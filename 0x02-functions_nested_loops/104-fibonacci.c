0cc090ee404a3579c3abd05ebefd88a18220cd1f
bernaba123 <70795558+bernaba123@users.noreply.github.com>
2021-02-09 03:38:17 -0800

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,10 +8,12 @@
  */
 int main(void)
 {
-int i, t1 = 1, t2 = 1, n;
+unsigned int i, t1 = 1, t2 = 1, n;
 
 for (i = 1; i < 99; i++)
 {
+if (i > 1)
+printf(", ");
 printf("%d", t1);
 n = t1 + t2;
 t1 = t2;
