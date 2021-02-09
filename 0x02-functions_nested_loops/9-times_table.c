a177eeb5fb16b4303a3eda7efc22560158032b66
bernaba123 <70795558+bernaba123@users.noreply.github.com>
2021-02-09 03:33:56 -0800

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,39 +1,43 @@
-#include "holberton"
+#include "holberton.h"
+
+int _putchar(char c);
+
 /**
- * void times_table - check the code for Holberton School students.
+ * times_table - print 9 table
  *
- * Return: Always 0.
+ * Return: No return
  */
+
 void times_table(void)
 {
-int i;
-int j;
-int n = 9;
+	int i;
+	int j;
+	int n = 9;
 
-for (i = 0; i <= n; i++)
-{
-for (j = 0; j <= n; j++)
-{
-if ((i * j) < 10)
-{
-if (j != 0)
-{
-_putchar(' ');
-_putchar(' ');
-}
-_putchar((j * i) + '0');
-}
-else if ((i * j) < 100)
-{
-_putchar(' ');
-_putchar((((j * i) / 10) % 10) + '0');
-_putchar(((j * i) % 10) + '0');
-}
-if (j != (n))
-{
-_putchar(',');
-}
-}
-_putchar('\n');
-}
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			if ((i * j) < 10)
+			{
+				if (j != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar((j * i) + '0');
+			}
+			else if ((i * j) < 100)
+			{
+				_putchar(' ');
+				_putchar((((j * i) / 10) % 10) + '0');
+				_putchar(((j * i) % 10) + '0');
+			}
+			if (j != (n))
+			{
+				_putchar(',');
+			}
+		}
+		_putchar('\n');
+	}
 }
