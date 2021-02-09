265a76b92721de917e899736ff27ef50a444be4d
bernaba123 <70795558+bernaba123@users.noreply.github.com>
2021-02-08 23:30:36 -0800

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,18 +8,19 @@
 
 void print_alphabet_x10(void)
 {
-int i;
-char c;
-  
-for (i = 0; i < 10; i++)
-{
-char c = 'a';
-  
-while (c <= 'z')
-{
-_putchar (c);
-c++;
-}
-_putchar ('\n');
-}
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		char alp = 'a';
+
+		while (alp <= 'z')
+		{
+
+		_putchar (alp);
+		alp++;
+
+		}
+		_putchar ('\n');
+	}
 }
