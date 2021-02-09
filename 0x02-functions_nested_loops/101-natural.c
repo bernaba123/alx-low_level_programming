5ae934297e2632252cd228a60f6451dbd139bb54
bernaba123 <70795558+bernaba123@users.noreply.github.com>
2021-02-09 02:20:43 -0800

    Create 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+int main(void){
+int S = 0, i; 
+for(i = 0; i < 1024; i++)
+if(i%3 == 0 || i%5 == 0)
+S = S+i; 
+printf("%d", S);
+return 0;
+}
