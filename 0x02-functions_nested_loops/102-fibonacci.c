57fe6c54a6a5e383df45a834bbd01b62527c25b8
bernaba123 <70795558+bernaba123@users.noreply.github.com>
2021-02-09 02:45:48 -0800

    Create 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int i, t1 = 1, t2 = 1, n;
+
+for (i = 1; i <= 50; i++)
+{
+printf("%d, ", t1);
+n = t1 + t2;
+t1 = t2;
+t2 = n;
+}
+return 0;
+}
