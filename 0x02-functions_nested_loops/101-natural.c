e80f838ee51f7a3105bede1e528881fdb028f06d
bernaba123 <70795558+bernaba123@users.noreply.github.com>
2021-02-09 05:34:46 -0800

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,18 +1,43 @@
-#include "holberton.h"
-#include <stdio.h
+#include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
+ * main - sums 3-5_cous - Entry point
  *
- * Return: Always 0.
+ * Return:  (0)
  */
 
 int main(void)
 {
-int S = 0, i;
-for (i = 0; i < 1024; i++)
-if (i % 3 == 0 || i % 5 == 0)
-S = S + i;
-printf("%d", S);
-return (0);
+	int i = 0;
+	int m3 = 3;
+	int m5 = 5;
+	int sum = 0;
+	int lim = 1024;
+
+	while (i == 0)
+	{
+
+		if (m3 < lim)
+		{
+			sum += m3;
+			m3 += 3;
+		}
+
+		if (m5 < lim && (m5 % 3) != 0)
+		{
+			sum += m5;
+			m5 += 5;
+		}
+		else if (m5 % 3 == 0)
+		{
+			m5 += 5;
+		}
+
+		if (m3 >= lim && m5 >= lim)
+		{
+			i = 1;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
 }
