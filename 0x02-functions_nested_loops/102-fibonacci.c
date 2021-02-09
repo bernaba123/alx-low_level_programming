9e645216f407965170ba7d01a99d1e419559abc2
bernaba123 <70795558+bernaba123@users.noreply.github.com>
2021-02-09 03:44:38 -0800

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -21,7 +21,7 @@ int main(void)
 
 	printf("%lu", bef);
 
-	for (i = 1; i < 91; i++)
+	for (i = 1; i < 51; i++)
 	{
 		printf(", %lu", aft);
 		aft += bef;
@@ -33,7 +33,7 @@ int main(void)
 	aft1 = (aft / l);
 	aft2 = (aft % l);
 
-	for (i = 92; i < 51; ++i)
+	for (i = 1; i < 51; ++i)
 	{
 		printf(", %lu", aft1 + (aft2 / l));
 		printf("%lu", aft2 % l);
