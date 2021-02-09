1c9ce5a718f9662fa93bef3bc05e9123b3627e6e
bernaba123 <bernabasgetachew0946@gmail.com>
2021-02-09 10:23:53 +0000

    all

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,11 +7,12 @@
  * Return: Always 0.
  */
 
-int main(void){
-int S = 0, i; 
-for(i = 0; i < 1024; i++)
-if(i%3 == 0 || i%5 == 0)
-S = S+i; 
+int main(void)
+{
+int S = 0, i;
+for (i = 0; i < 1024; i++)
+if (i % 3 == 0 || i % 5 == 0)
+S = S + i;
 printf("%d", S);
-return 0;
+return (0);
 }
