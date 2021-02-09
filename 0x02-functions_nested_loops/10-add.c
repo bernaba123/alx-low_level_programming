9ade720a7e733d8d6caea690512ab568b7f0c8b4
bernaba123 <bernabasgetachew0946@gmail.com>
2021-02-09 08:27:00 +0000

    all

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,8 +1,18 @@
 #include "holberton.h"
 
+/**
+ * add - Entry.
+ *
+ * @i: an integer argument
+ *
+ * @j: an integer argument
+ *
+ * Return: Always 0.
+ */
 
-int add(int i, int j){
+int add(int i, int j)
+{
 int result;
 result = i + j;
-return result;
+return (result);
 }
