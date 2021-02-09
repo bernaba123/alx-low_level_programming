a17945904c8f591670e19a3a618553085f6758d9
bernaba123 <70795558+bernaba123@users.noreply.github.com>
2021-02-09 03:46:08 -0800

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - fibonacci <3
+ * main - fibonacci 50
  *
- * Purpose - no hardcode
  *
  * Return:  (Success)
  */
@@ -11,36 +10,16 @@
 int main(void)
 {
 	unsigned long int i;
-	unsigned long int bef = 1;
-	unsigned long int aft = 2;
-	unsigned long int l = 1000000000;
-	unsigned long int bef1;
-	unsigned long int bef2;
-	unsigned long int aft1;
-	unsigned long int aft2;
+	unsigned long int b = 1;
+	unsigned long int a = 2;
 
-	printf("%lu", bef);
+	printf("%lu", b);
 
-	for (i = 1; i < 51; i++)
+	for (i = 1; i < 50; i++)
 	{
-		printf(", %lu", aft);
-		aft += bef;
-		bef = aft - bef;
-	}
-
-	bef1 = (bef / l);
-	bef2 = (bef % l);
-	aft1 = (aft / l);
-	aft2 = (aft % l);
-
-	for (i = 1; i < 51; ++i)
-	{
-		printf(", %lu", aft1 + (aft2 / l));
-		printf("%lu", aft2 % l);
-		aft1 = aft1 + bef1;
-		bef1 = aft1 - bef1;
-		aft2 = aft2 + bef2;
-		bef2 = aft2 - bef2;
+		printf(", %lu", a);
+		a += b;
+		b = a - b;
 	}
 	printf("\n");
 	return (0);
