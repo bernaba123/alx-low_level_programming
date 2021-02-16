41b68c1b876daebd0b90e95e856abd2393710420
bernaba123 <bernabasgetachew0946@gmail.com>
2021-02-16 05:31:41 +0000

    all

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,5 +1,11 @@
 #include "holberton.h"
-
+/**
+ * _strlen - check the code for Holberton School students.
+ *
+ * @s: is a character param
+ *
+ * Return: Always 0.
+ */
 int _strlen(char *s)
 {
 return (strlen(s));
