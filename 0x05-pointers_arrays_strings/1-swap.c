e1b125f620d70f340eefefba27aaf3b80a20a0d5
bernaba123 <bernabasgetachew0946@gmail.com>
2021-02-16 05:27:59 +0000

    all

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,5 +1,13 @@
 #include "holberton.h"
-
+/**
+ * swap_int - prints a triangle
+ *
+ * @a: an integer params
+ *
+ * @b: an integer params
+ *
+ * void function
+ */
 void swap_int(int *a, int *b)
 {
 int t;
