f24a7696e23c80a29689820c416ce814271dc6e0
bernaba123 <70795558+bernaba123@users.noreply.github.com>
2021-02-18 12:32:33 +0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,6 +11,7 @@ char *_strcat(char *dest, char *src)
 {
 int i
 int j;
+
 i = 0;
 j = 0;
 
@@ -18,12 +19,14 @@ while (dest[i] != '\0')
 {
 i++;
 }
+  
 while (src[j] != '\0')
 {
 dest[i] = src[j];
 j++;
 i++;
 }
+  
 dest[i] = '\0';
 return (dest);
 }
