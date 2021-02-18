683df15eac0115bb5e1ed4edbd8d48e601f90ec2
bernaba123 <70795558+bernaba123@users.noreply.github.com>
2021-02-18 12:21:58 +0300

    Update holberton.h

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,22 +1,22 @@
 #ifndef FILE_HOLBERTON
 #define FILE_HOLBERTON
 
-char *_strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src);
 
-char *_strncat(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n);
 
-char *_strncpy(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n);
 
-int _strcmp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2);
 
-void reverse_array(int *a, int n)
+void reverse_array(int *a, int n);
 
-char *string_toupper(char *)
+char *string_toupper(char *);
 
-char *cap_string(char *)
+char *cap_string(char *);
 
-char *leet(char *)
+char *leet(char *);
 
-char *rot13(char *)
+char *rot13(char *);
 
 #endif
