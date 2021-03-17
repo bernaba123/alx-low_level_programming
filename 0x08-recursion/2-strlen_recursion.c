c51fe9ba32bc6309a19b1d0ac654c822de478a16
bernaba123 <70795558+bernaba123@users.noreply.github.com>
2021-03-17 13:06:47 +0300

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,10 +8,10 @@
 
 int _strlen_recursion(char *s)
 {
-	int i = 0; 
-	if (*s != '\0')
-	{
-		i++;
-		return i + _strlen_recursion(s + i);
-	}
+int i = 0; 
+if (*s != '\0')
+{
+i++;
+return i + _strlen_recursion(s + i);
+}
 }
